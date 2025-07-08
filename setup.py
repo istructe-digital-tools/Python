6633133152510ce802f18e9a2e150eb2b041f13b
istructe-digital-tools <186558821+istructe-digital-tools@users.noreply.github.com>
2025-07-08 10:47:10 +0100

    Include Setup Files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DesignCodes",
+    version="0.1.0",
+    author="IStructE",
+    author_email="@",
+    description="A Python project for engineering calculations and design equations.",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    license="Fair Source License",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
